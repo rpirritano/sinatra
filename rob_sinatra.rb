e893a1ef9c3c4eaa4ec9226abf3680c7e61ada85
Robert Pirritano <rpirritano@gmail.com>
2017-08-18 11:17:30 -0700

    handled error - no path

--- rob_sinatra.rb
+++ rob_sinatra.rb
@@ -1,5 +1,7 @@
 require "rack"
 
+
+
 module Rob
   class Base
     def initialize
@@ -11,6 +13,27 @@ module Rob
     def get(path, &handler)
       route("GET", path, &handler)
     end
+
+    #to make a Rack app by adding a minimal call method
+    def call(env)
+      @request = Rack::Request.new(env)
+      verb = @request.request_method
+      requested_path = @request.path_info
+#handle error of no route
+      handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
+
+      if handler
+        handler.call
+      else
+        [404, {}, ["Oops! No rote for #{verb} #{requested_path}"]]
+      end
+    end
+
+=begin
+    First, we grab the verb and requested path (like GET and /the/path)
+    from the env parameter using Rack::Request.
+    Then we grab the handler block from @routes and call it.
+=end
 
     private
     def route(verb, path, &handler)
@@ -27,4 +50,5 @@ rob.get "/hello" do
   [200, {}, ["Robert says hello"]]
 end
 
-puts rob.routes
+#add the handler now to run it
+Rack::Handler::WEBrick.run rob, Port: 9292
